2112e8aa72614f6b2843c280b5a2976ae3071135
Allef <allefmiller@hotmail.com>
2022-10-29 15:32:28 -0300

    inicial commit

---
+++ trem.cpp
@@ -0,0 +1,61 @@
+#include "trem.h"
+#include <QtCore>
+
+//Construtor
+Trem::Trem(int ID, int x, int y){
+    this->ID = ID;
+    this->x = x;
+    this->y = y;
+    velocidade = 100;
+}
+
+//Função a ser executada após executar trem->START
+void Trem::run(){
+    int c = 0;
+    while(true){
+        switch(ID){
+        case 1:     //Trem 1
+            if (y == 30 && x <330)
+                x+=10;
+            else if (x == 330 && y < 150)
+                y+=10;
+            else if (x > 60 && y == 150)
+                x-=10;
+            else
+                y-=10;
+            emit updateGUI(ID, x,y);    //Emite um sinal
+            break;
+        case 2: //Trem 2
+            if (y == 30 && x <600)
+                x+=10;
+            else if (x == 600 && y < 150)
+                y+=10;
+            else if (x > 330 && y == 150)
+                x-=10;
+            else
+                y-=10;
+            emit updateGUI(ID, x,y);    //Emite um sinal
+            break;
+        case 3: //Trem 3
+            if(x > 470)
+                c++;
+            if (y == 40 && x < 483) // andar para a direitas
+                x+=10;
+            else if (x == 483 && y < 220) // andar para baixo
+                y+=10;
+            else if (x > 40 && y == 220) // andar para a esquerda
+                x-=10;
+            else
+                y-=10; // andar para cima
+            emit updateGUI(ID, x,y);    //Emite um sinal
+            break;
+        default:
+            break;
+        }
+        msleep(velocidade);
+    }
+}
+
+
+
+
